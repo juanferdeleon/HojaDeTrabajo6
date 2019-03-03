8142ae59579e45970dc9e80323726e5018ed2fa7
Juan De Leon <dele17822@uvg.edu.gt>
2019-03-03 13:00:32 -0600

    Card class

---
+++ src/Card.java
@@ -0,0 +1,26 @@
+/**
+ * Card class
+ * @author Juanfer De Leon
+ */
+public class Card {
+
+    private String cardName;
+    private String cardType;
+
+    public String getCardName() {
+        return cardName;
+    }
+
+    public void setCardName(String cardName) {
+        this.cardName = cardName;
+    }
+
+    public String getCardType() {
+        return cardType;
+    }
+
+    public void setCardType(String cardType) {
+        this.cardType = cardType;
+    }
+
+}
